422ab5454ce277353575b9834dc61afb19eeaef0
Vera Bylinskaia <iagyar@list.ru>
2024-03-21 01:38:33 +0300

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 1: Задайте значения M и N. Напишите программу, которая выведет
+все натуральные числа в промежутке от M до N.
+Использовать рекурсию, не использовать циклы.
+*/
+
+void GetSumDigit(int n, int m){
+    if (n < m){
+        return;
+    }
+    GetSumDigit(n - 1, m);
+    Console.Write(n+" ");
+}
+
+Console.Clear();
+int m = 2;
+int n = 8;
+GetSumDigit (n, m);
