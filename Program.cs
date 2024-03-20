72ef84692ee2f8e6e569a529219f8cdbb418c068
Vera Bylinskaia <iagyar@list.ru>
2024-03-21 01:41:13 +0300

    Задача 3

--- Program.cs
+++ Program.cs
@@ -23,19 +23,37 @@
 с помощью рекурсии. Даны два неотрицательных числа m и n.
 */
 
-int Akkerman(int m, int n){
-    if (m == 0){
-        return n + 1;
+// int Akkerman(int m, int n){
+//     if (m == 0){
+//         return n + 1;
+//     }
+//     else if (n == 0){
+//         return Akkerman(m - 1, 1);
+//     }
+//     else {
+//         return Akkerman(m - 1, Akkerman(m, n - 1));
+//     }
+// }
+
+// Console.Clear();
+// int m = 3;
+// int n = 2;
+// Console.Write(Akkerman(m, n));
+
+
+/*
+Задача 3: Задайте произвольный массив. Выведете
+его элементы, начиная с конца. Использовать
+рекурсию, не использовать циклы.
+*/
+
+void ShowArray(int [] array, int index){
+    if (index < 0){
+        return;
     }
-    else if (n == 0){
-        return Akkerman(m - 1, 1);
-    }
-    else {
-        return Akkerman(m - 1, Akkerman(m, n - 1));
-    }
+    Console.Write($"{array[index]} ");
+    ShowArray(array, index - 1);
 }
 
-Console.Clear();
-int m = 3;
-int n = 2;
-Console.Write(Akkerman(m, n));
+int [] array = new int [] {1, 2, 5, 6, 9};
+ShowArray (array, array.Length - 1);
